e749024650e87d2df407636ec73384726e1b29bd
Harriso Owede <harrisoowede@Harrisos-MacBook-Air.local>
2023-11-07 12:23:20 +0100

    firstcommit

--- src/Component/FirstLanding.jsx
+++ src/Component/FirstLanding.jsx
@@ -8,6 +8,7 @@ const FirstLanding = () => {
        <img className='w-full h-full object-cover' src={background} alt="background" />
        <div className='flex flex-col items-center absolute h-full w-full justify-center top-0'>
         <img className="pb-[150px] flex pl-4 w-50% h-50%" src={logo} alt="logo" />
+        
 
         <div className='pl-3'>
           <p className='text-xl sm:text-xl md:text-4xl text-center'>Empowering Your Voice With Vortech</p> 
